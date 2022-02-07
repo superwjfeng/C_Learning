fe9ca24239808f1283364bdc080ef34f3aadd9c8
WeijianFeng <wj.feng@tum.de>
2022-02-07 21:23:17 +0100

    2022-02-07

--- Homework/Homework_12/Source.c
+++ Homework/Homework_12/Source.c
@@ -1,19 +1,81 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+////Problem 6
+//int main()
+//{
+//	int n, m = 0;
+//	scanf("%d%d", &n, &m);
+//	int i = 0;
+//	int count = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		int m_1bit = (m >> i) & 1;
+//		int n_1bit = (n >> i) & 1;
+//		if (1 == (m_1bit ^ n_1bit))
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+
+////Problem 7
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int num_even, num_odd = 0;
+//
+//	printf("Even:\n");
+//	for (i = 31; i > 0; i -= 2)
+//	{
+//		num_odd = (n >> i) & 1;
+//		printf("%d ", num_odd);
+//	}
+//
+//	printf("\n");
+//	printf("Odd\n");
+//
+//	for (i = 30; i >= 0; i -= 2)
+//	{
+//		num_even = (n >> i) & 1;
+//		printf("%d ", num_even);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1; i <= 32; i++)
+//	{
+//		if ((n >> i) & 1)
+//			count++;
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+void swap(int a, int b)
+{
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d, %d", a, b);
+}
+
 int main()
 {
-	int n, m = 0;
-	scanf("%d%d", &n, &m);
-	int i = 0;
-	int count = 0;
-	for (i = 0; i < 32; i++)
-	{
-		if ((m >> i) ^ (n >> i))
-		{
-			count++;
-		}
-	}
-	printf("Count=%d\n", count);
+	int a, b = 0;
+	scanf("%d%d", &a, &b);
+	swap(a,b);
 	return 0;
 }
---
+++ Homework/Homework_13/Source.c
@@ -0,0 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+	return 0;
+}
