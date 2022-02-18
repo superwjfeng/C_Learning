5bf0837b922e69ccda6ad990e50032c226cdebcd
WeijianFeng <wj.feng@tum.de>
2022-02-18 16:37:19 +0100

    2022.02,18

---
+++ Lecture/Lecture15/Source.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+struct Stu
+{
+	char name[20];
+	int age;
+	char sex[8];
+	float score;
+}s1,s2,s3; //s1,s2,s3是通过struct Stu类型创建的变量
+//s1,s2,s3是全局变量
+
+int main()
+{
+	return 0;
+}
